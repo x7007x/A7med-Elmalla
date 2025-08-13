8e5f2700a0ceb73ce972464f3bd47e8aedbea76a
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-08-13 21:42:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask import Flask, request, Response
 import requests
 
 app = Flask(__name__)
-
-TARGET = "https://brave-terese-uuuuuu7gco-4c4942e9.koyeb.app/"
+TARGET = "https://brave-terese-uuuuuu7gco-4c4942e9.koyeb.app"
 
 @app.route('/', defaults={'path': ''}, methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
 @app.route('/<path:path>', methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
 def proxy(path):
     url = f"{TARGET}/{path}"
-    headers = {key: value for key, value in request.headers if key.lower() != 'host'}
+    headers = {k: v for k, v in request.headers if k.lower() != 'host'}
     resp = requests.request(
         method=request.method,
         url=url,
@@ -18,8 +17,7 @@ def proxy(path):
         cookies=request.cookies,
         allow_redirects=False
     )
-    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    response_headers = [(name, value) for name, value in resp.headers.items() if name.lower() not in excluded_headers]
+    response_headers = [(name, value) for name, value in resp.headers.items()]
     return Response(resp.content, resp.status_code, response_headers)
 
 if __name__ == "__main__":
