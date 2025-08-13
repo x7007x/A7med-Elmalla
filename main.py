26e643d243eb33db5fcb0a0b36d9f0881dc2b7ac
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-08-13 21:44:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ TARGET = "https://brave-terese-uuuuuu7gco-4c4942e9.koyeb.app"
 def proxy(path):
     url = f"{TARGET}/{path}"
     headers = {k: v for k, v in request.headers if k.lower() != 'host'}
+    body = request.get_data() if request.method not in ["GET", "HEAD"] else None
     resp = requests.request(
         method=request.method,
         url=url,
         headers=headers,
-        data=request.get_data(),
+        data=body,
         cookies=request.cookies,
         allow_redirects=False
     )
-    response_headers = [(name, value) for name, value in resp.headers.items()]
+    excluded_headers = ['transfer-encoding', 'connection']
+    response_headers = [(name, value) for name, value in resp.headers.items() if name.lower() not in excluded_headers]
     return Response(resp.content, resp.status_code, response_headers)
-
-if __name__ == "__main__":
-    app.run()
